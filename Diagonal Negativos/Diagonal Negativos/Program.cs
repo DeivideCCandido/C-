0c3b13e16dc24c0f486a82bbae9e24082d921078
DeivideCCandido <134802248+DeivideCCandido@users.noreply.github.com>
2023-07-31 20:20:33 -0300

    Linguagem C#
    Aulas de programação Udemy

---
+++ Diagonal Negativos/Diagonal Negativos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Programa {
+    class Program {
+        static void Main(string[] args) {
+
+            int N, i, j, negativos;
+
+            Console.Write("Qual a ordem da matriz ? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[N, N];
+
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < N; j++) {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL: ");
+            for (i = 0; i < N; i++) {
+                Console.Write(mat[i, i] + "  ");
+            }
+
+            negativos = 0;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < N; j++) {
+                    if (mat[i, j] < 0) {
+                        negativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nQUANTIDADE DE NEGATIVOS = " + negativos);
+        }
+    }
+}
+
+
+
+
+
+        
+    
+
